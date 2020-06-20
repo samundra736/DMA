6e84ae2b8b8a1bf95e15765782f9f90200869acb
samundra736 <61288336+samundra736@users.noreply.github.com>
2020-06-20 19:22:13 +0545

    Add files via upload

---
+++ androidtodo/app/src/main/java/com/example/todomvvm/addedittask/AddEditTaskViewModel.java
@@ -0,0 +1,37 @@
+package com.example.todomvvm.addedittask;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.todomvvm.database.AppDatabase;
+import com.example.todomvvm.database.Repository;
+import com.example.todomvvm.database.TaskEntry;
+
+public class AddEditTaskViewModel extends AndroidViewModel {
+
+    Repository repository;
+    LiveData<TaskEntry> task;
+
+    AddEditTaskViewModel(Application application, int taskId){
+        super(application);
+        AppDatabase database = AppDatabase.getInstance(application);
+    }
+
+
+    public LiveData<TaskEntry> getTask(){
+        return task;
+    }
+
+    public void insertTask(TaskEntry task){
+        repository.insertTask(task);
+    }
+
+    public void updateTask(TaskEntry task){
+        repository.insertTask(task);
+    }
+
+
+}
